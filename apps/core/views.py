8a84e5f7b488f5eb02d9e5ee468815cabc8e468d
charlie <jhdhj999@gmail.com>
2025-01-16 00:44:54 +0600

    Display username

--- apps/core/views.py
+++ apps/core/views.py
@@ -1,4 +1,9 @@
-from django.shortcuts import render
+from django.contrib.auth import login
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, redirect
+
+from apps.userprofile.models import Userprofile
 
 def frontpage(request):
     return render(request, "core/frontpage.html")
@@ -11,3 +16,21 @@ def terms(request):
 
 def plans(request):
     return render(request, 'core/plans.html')
+
+def signup(request):
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        
+        if form.is_valid():
+            user = form.save()
+            user.email = user.username
+            user.save()
+            
+            userprofile = Userprofile.objects.create(user=user)
+            
+            login(request, user)
+            
+            return redirect("frontpage")
+    else:
+        form = UserCreationForm
+    return render(request, "core/signup.html", {"form": form})
--- apps/userprofile/views.py
+++ apps/userprofile/views.py
@@ -1,11 +1,9 @@
 from django.contrib import messages
-from django.contrib.auth import login
 from django.contrib.auth.decorators import login_required
-from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from django.shortcuts import render, redirect
 
-from .models import Userprofile
+#from .models import Userprofile
 
 @login_required
 def myaccount(request):
@@ -24,20 +22,3 @@ def edit_profile(request):
         return redirect("myaccount")
     return render(request, "userprofile/edit_profile.html")
     
-def signup(request):
-    if request.method == "POST":
-        form = UserCreationForm(request.POST)
-        
-        if form.is_valid():
-            user = form.save()
-            user.email = user.username
-            user.save()
-            
-            userprofile = Userprofile.objects.create(user=user)
-            
-            login(request, user)
-            
-            return redirect("frontpage")
-    else:
-        form = UserCreationForm
-    return render(request, "userprofile/signup.html", {"form": form})
